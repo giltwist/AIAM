6159be5b9e65cdac3175b3e01bf8670a622ad5bd
giltwist <giltwist@gmail.com>
2025-10-06 12:16:11 -0400

    Remove text cleaning
    Difference was negligible

--- reddit_user_IDF.py
+++ reddit_user_IDF.py
@@ -14,47 +14,38 @@ import gensim
 import pandas as pd
 import time
 
-df=load_dataset(red_row=10000, user_row=1000)
-# Clean data using the built in cleaner in gensim
-df['text_clean'] = df['text'].apply(lambda x: " ".join(gensim.utils.simple_preprocess(x)))
+df=load_dataset(red_row=20000, user_row=2000)
 
 print("Dataset size:", len(df))
-
-
 
 # Initialize TF-IDF Vectorizer
 vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
 # Transform the text data to feature vectors
-X_unclean = vectorizer.fit_transform(df['text'])
-X_clean = vectorizer.fit_transform(df['text_clean'])
+X = vectorizer.fit_transform(df['text'])
 # Labels
 y = df['label']
 
-X_sets=[X_unclean,X_clean]
+print (f'Beginning attempt {trial}')
+# Split the dataset into training and testing sets
+train_start = time.time()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-for trial, X in enumerate(X_sets):
+# Initialize and train the classifier
+# only rbf had better accuracy but at double time
+clf = SVC(kernel='linear')
+clf.fit(X_train, y_train)
+train_end=time.time();
 
-    print (f'Beginning attempt {trial}')
-    # Split the dataset into training and testing sets
-    train_start = time.time()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+print("\tTraining time: ", train_end-train_start)
 
-    # Initialize and train the classifier
-    # only rbf had better accuracy but at double time
-    clf = SVC(kernel='linear')
-    clf.fit(X_train, y_train)
-    train_end=time.time();
+# Predict on the test set
+y_pred = clf.predict(X_test)
+predict_end = time.time()
 
-    print("\tTraining time: ", train_end-train_start)
-
-    # Predict on the test set
-    y_pred = clf.predict(X_test)
-    predict_end = time.time()
-
-    # Evaluate the performance
-    accuracy = accuracy_score(y_test, y_pred)
-    report = classification_report(y_test, y_pred, target_names=['reddit','user'])
-    report_end=time.time()
+# Evaluate the performance
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred, target_names=['reddit','user'])
+report_end=time.time()
 
     print(f'\tAccuracy: {accuracy:.4f} \n')
     #print('Classification Report:')
