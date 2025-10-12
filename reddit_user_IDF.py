efda81fa48084d8b920bb7c00cd77e4fc75db71b
giltwist <giltwist@gmail.com>
2025-10-12 13:29:52 -0400

    Model/vectorizer saving/loading, and GUI
    Minimum viable product for class

--- reddit_user_IDF.py
+++ reddit_user_IDF.py
@@ -2,7 +2,7 @@
 # Kernel testing insight from https://sklearner.com/sklearn-svc-kernel-parameter/
 # gensim additions from https://medium.com/@dilip.voleti/classification-using-word2vec-b1d79d375381
 
-from reddit_user_common import load_dataset
+from reddit_user_common import load_dataset, tk_init, show_result
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -14,39 +14,68 @@ import gensim
 import pandas as pd
 import time
 
-df=load_dataset(red_row=20000, user_row=20000)
+import joblib
+import tkinter as tk
 
-print("Dataset size:", len(df))
+class RU_IDF:
 
-# Initialize TF-IDF Vectorizer
-vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7, ngram_range=(1,3))
-# Transform the text data to feature vectors
-X = vectorizer.fit_transform(df['text'])
-# Labels
-y = df['label']
+    def train_idf(self, verbose):
+        df=load_dataset(red_row=20000, user_row=20000)
 
-# Split the dataset into training and testing sets
-train_start = time.time()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+        print("Dataset size:", len(df))
 
-# Initialize and train the classifier
-# only rbf had better accuracy but at double time
-clf = SVC(kernel='linear')
-clf.fit(X_train, y_train)
-train_end=time.time();
+        # Initialize TF-IDF Vectorizer
+        vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7, ngram_range=(1,3))
+        # Transform the text data to feature vectors
+        X = vectorizer.fit_transform(df['text'])
+        # Labels
+        y = df['label']
 
-print("\tTraining time: ", train_end-train_start)
+        # Split the dataset into training and testing sets
+        train_start = time.time()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Predict on the test set
-y_pred = clf.predict(X_test)
-predict_end = time.time()
+        # Initialize and train the classifier
+        # only rbf had better accuracy but at double time
+        idf = SVC(kernel='linear')
+        idf.fit(X_train, y_train)
+        train_end=time.time();
 
-# Evaluate the performance
-accuracy = accuracy_score(y_test, y_pred)
-report = classification_report(y_test, y_pred, target_names=['reddit','user'])
-report_end=time.time()
+        print("\tTraining time: ", train_end-train_start)
 
-print(f'\tAccuracy: {accuracy:.4f} \n')
-print('Classification Report:')
-print(report)
+        if verbose:
+            # Predict on the test set
+            y_pred = idf.predict(X_test)
+            predict_end = time.time()
+
+            # Evaluate the performance
+            accuracy = accuracy_score(y_test, y_pred)
+            report = classification_report(y_test, y_pred, target_names=['reddit','user'])
+            report_end=time.time()
+
+            print(f'\tAccuracy: {accuracy:.4f} \n')
+            print('Classification Report:')
+            print(report)
+
+        joblib.dump(idf,'./datasets/tfidf-model.pkl')
+        joblib.dump(vectorizer,'./datasets/tfidf-vectorizer.pkl')
+        return idf, vectorizer
+    
+    def predict(self):
+        X = self.vectorizer.transform([self.text_entry.get("1.0", tk.END)])
+        prediction = self.model.predict(X)
+        show_result(self,prediction[0]=='user')
+
+
+if __name__ == "__main__":
+    ru = RU_IDF()
+    try:
+        ru.model=joblib.load('./datasets/tfidf-model.pkl')
+        ru.vectorizer=joblib.load('./datasets/tfidf-vectorizer.pkl')
+        print("Loading existing model")
+    except:
+        print("Generating new model")
+        ru.model, ru.vectorizer = ru.train_idf(True)
+
+    tk_init(ru,ru.predict)
 
